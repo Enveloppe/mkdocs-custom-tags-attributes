702dddf8490bfd82fe8cdd8b77ce65ea46373a0c
Mara <mara-li@outlook.fr>
2022-06-28 02:36:46 +0200

    feat: create the plugin

---
+++ custom_attributes/plugin.py
@@ -0,0 +1,70 @@
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+import re
+from pathlib import Path
+
+
+def read_custom(config: dict, custom: str):
+    css_file = Path(config.get('docs_dir'), custom)
+    css = []
+    with open(css_file, 'r', encoding='utf-8') as custom_attr:
+        for i in custom_attr.readlines():
+            if i.startswith('#'):
+                css.append(i.replace('{\n', '').strip())
+    return css
+
+
+def convert_hashtags(config: dict, line: str, custom_configuration:str) -> str:
+    css = read_custom(config, custom_configuration)
+    token = re.findall(r'#\S+', line)
+    token = list(set(token))
+    for i in range(0, len(token)):
+        tags = token[i]
+        if tags in css:
+            original_line = line
+            line = line.replace(tags, '')
+            if len(line.strip()) == 0:
+                return original_line
+            if line.startswith('#'):
+                heading = re.findall('#', line)
+                heading = "".join(heading)
+                ial = (
+                        heading + ' **' + line.replace('#', '').strip()
+                        + '**{: ' + tags + '}'
+                )
+            else:
+                ial = '**' + line.strip() + '**{: ' + tags + '}'
+            line = line.replace(line, ial)
+        else:
+            ial = (
+                    '**'
+                    + tags.replace('#', ' ').strip()
+                    + '**{: '
+                    + tags.strip()
+                    + ' .hash}'
+            )
+            line = line.replace(token[i], ial, 1)
+    return line
+
+
+class CalloutsPlugin(BasePlugin):
+    """
+    Reads the files and convert #tags to **tags**{: #tags .hash}
+    Convert to id if found in configuration files
+    """
+    config_scheme = (
+        ('file', config_options.Type(str, default='assets/css/custom_attributes.css')),
+    )
+
+    def on_page_markdown(self, markdown, page, config, files):
+        files_contents = markdown.split('\n')
+        custom_config = self.config['file']
+        markdown = ''
+        for line in files_contents:
+            if re.search(r'#\w+', line) and not re.search(
+                    r'(`|\[{2}|\()(.*)#(.*)(`|\]{2}|\))', line
+            ):
+                line = convert_hashtags(config, line, custom_config)
+            markdown += line + '\n'
+        return markdown
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '0.0.1'
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='mkdocs-custom-tags-attributes',
+    version=version,
+    description='Adding custom attributes using hashtags.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    utl='https://github.com/Mara-Li/mkdocs-custom-tags-attributes',
+    author='Mara-Li',
+    author_email='mara-li@outlook.fr',
+    license='GPL-3',
+    python_requires='>=3.10',
+    install_requires=required,
+    tests_require=['pytest'],
+    packages=find_packages(),
+    keywords="mkdocs, custom attributes, pymdownx, markdown extension, markdown, md",
+    entry_points={'mkdocs.plugins': ['custom-attributes = custom_attributes.plugin:CalloutsPlugin']}
+)
