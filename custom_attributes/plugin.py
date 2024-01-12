ae39f717b5d4a522b706721e4866df9019003c5d
Mara <mara-li@outlook.fr>
2024-01-12 13:15:11 +0100

    fix: add better way to exclude tags in some text like links, wikilinks, code, and html
    also fix wrong read html

--- custom_attributes/plugin.py
+++ custom_attributes/plugin.py
@@ -1,8 +1,10 @@
 """Main script that convert the #hashtags of a Markdown text during the on_page_markdown event."""
 
+import logging
 import re
 from pathlib import Path
-from typing import List, Dict
+from typing import Dict, List, Optional
+
 from mkdocs.config import Config, config_options
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import Files
@@ -11,26 +13,51 @@ from mkdocs.structure.pages import Page
 
 def read_custom(config: Dict[str, str]) -> List[str]:
     """Read the css file and take each css ID selector and return it as a list."""
-    css_file = Path(config.get("docs_dir"), config.get("file"))
+    css_file = Path(config.get("docs_dir"), config.get("file"))  # type: ignore
     try:
-        with open(css_file, "r", encoding="utf-8") as custom_attr:
+        css_file_path = Path(css_file)
+        with css_file_path.open("r", encoding="utf-8") as custom_attr:
             custom_css = custom_attr.read()
         custom_css = re.sub(r"[\t\n]", "", custom_css)
         custom_css = re.sub(r"\{.*?\}", ",", custom_css)
+        custom_css = re.sub(r"\/\*.*?\*\/", "", custom_css, flags=re.DOTALL)
         custom_css = custom_css.split(",")
         return [i.strip() for i in custom_css if i.strip().startswith("#")]
     except FileNotFoundError:
-        print("No CSS configured.")
+        logging.getLogger(__name__)
+        logging.warning(
+            "The file %s was not found. Please check the path and try again.",
+            css_file,
+        )
         return []
 
 
 def cleanned_word(line: str, word_regex: str) -> str:
     """Check the word before the attributes tags"""
-    if re.search(word_regex, line):
-        word_before_tags = re.search(word_regex, line).group().strip()
+    search = re.search(word_regex, line)
+    if search:
+        word_before_tags = search.group().strip()
     else:
         word_before_tags = ""
     return word_before_tags
+
+
+def is_excluded(tags: Optional[str], line: str) -> bool:
+    if not tags:
+        return False
+    link_regex = r"\[\[?(.*)" + re.escape(tags) + r"(.*)\]\]?"
+    if re.search(link_regex, line):
+        return True
+    code_regex = r"`(.*)" + re.escape(tags) + r"(.*)`"
+    if re.search(code_regex, line):
+        return True
+    html_regex = r"<(.*)" + re.escape(tags) + r"(.*)>"
+    if re.search(html_regex, line):
+        return True
+    parenthesis_regex = r"\((.*)" + re.escape(tags) + r"(.*)\)"
+    if re.search(parenthesis_regex, line):
+        return True
+    return False
 
 
 def convert_hashtags(config: Dict[str, str], line: str) -> str:
@@ -47,9 +74,9 @@ def convert_hashtags(config: Dict[str, str], line: str) -> str:
                 return line
             markup = "**{: " + tag + "}"
             word_regex = r"\w+" + re.escape(tag)
-
-            if line.startswith("#"):
-                heading = re.search("^#*", line).group() + " "
+            startswith = re.search("^#*", line)
+            if line.startswith("#") and startswith:
+                heading = startswith.group() + " "
                 without_heading = re.sub("^#*", "", line).strip()
                 word_before_tags = cleanned_word(without_heading, word_regex)
 
@@ -89,6 +116,9 @@ def convert_text_attributes(markdown: str, config: Dict[str, str]) -> str:
     markdown = ""
     code_blocks = False
     for line in files_contents:
+        tags = re.search(r"#[\w\-_\/]+", line)
+        if tags:
+            tags = tags.group()
         if not code_blocks and (
             line.startswith("```")
             or (re.search("<.*?>", line) and not re.search("</.*?>", line))
@@ -103,25 +133,27 @@ def convert_text_attributes(markdown: str, config: Dict[str, str]) -> str:
             code_blocks = False
 
         elif (
-            re.search(r"#\w+", line)
-            and not re.search(r"(`|\[{2}|\()(.*)#(.*)(`|]{2}|\))", line)
-            and not code_blocks
+            re.search(r"#\w+", line) and not is_excluded(tags, line) and not code_blocks
         ):
-            line = convert_hashtags(config, line)
+            line = convert_hashtags(config, line)  # noqa
         markdown += line + "\n"
     return markdown
 
 
 class TagsAttributePlugins(BasePlugin):
-    """Reads the files and convert #tags to **tags**{: #tags .hash} Convert to id/attributes list if found in configuration files."""
+    """Reads the files and convert #tags to **tags**{: #tags .hash} Convert to id/attributes list if found in configuration files."""  # noqa: E501
 
     config_scheme = (
         ("file", config_options.Type(str, default="assets/css/custom_attributes.css")),
     )
 
     def on_page_markdown(
-        self, markdown: str, page: Page, config: Config, files: Files
+        self,  # noqa
+        markdown: str,
+        page: Page,
+        config: Config,
+        files: Files,
     ) -> str:
         """Run the conversion based on the page_markdown event."""
-        config = {"docs_dir": config["docs_dir"], "file": self.config["file"]}
-        return convert_text_attributes(markdown, config)
+        config = {"docs_dir": config["docs_dir"], "file": self.config["file"]}  # type: ignore
+        return convert_text_attributes(markdown, config)  # type: ignore
